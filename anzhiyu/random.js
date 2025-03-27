ba6da7b9df32f5b5ffff821b4d4f0530a26596a8
LonelyOutline <1977194518@qq.com>
2025-03-27 16:11:32 +0800

    Site updated: 2025-03-27 16:11:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/24/hello-world/","2025/03/25/发布的第一篇文章/"];function toRandomPost(){
+var posts=["2025/03/24/hello-world/","2025/03/26/关于博客的一些设置/","2025/03/25/发布的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
