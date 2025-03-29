5c3fef1c23fd486170bbbe4b48b6b286d2deb356
LonelyOutline <1977194518@qq.com>
2025-03-29 22:29:36 +0800

    Site updated: 2025-03-29 22:28:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/27/htlm基础内容-基础结构、知识）/","2025/03/25/发布的第一篇文章/","2025/03/28/碎碎念/","2025/03/27/关于博客/","2025/03/27/前端JS：关于DOM元素/"];function toRandomPost(){
+var posts=["2025/03/27/htlm基础内容-基础结构、知识）/","2025/03/27/关于博客/","2025/03/27/前端JS：关于DOM元素/","2025/03/25/发布的第一篇文章/","2025/03/28/碎碎念/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
